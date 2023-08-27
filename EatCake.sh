7fef346b96cc7ac04c655d34fc76bf90784b4032
Albir Tarsha <a.tarsha@yahoo.com>
2023-08-26 20:54:43 -0500

    Added FindDuplicate,SpaceEditionTM_BEAST-MODE

--- EatCake.sh
+++ EatCake.sh
@@ -4,3 +4,5 @@ printf "=== MeshMessage\n"
 node MeshMessage/MeshMessage.1.0.js
 printf "\n=== RecursiveStringPermutations\n"
 node RecursiveStringPermutations/RecursiveStringPermutations.1.0.js
+printf "\n=== FindDuplicate,SpaceEditionTM_BEAST-MODE\n"
+node FindDuplicate,SpaceEditionTM_BEAST-MODE/FindDuplicate,SpaceEditionTM_BEAST-MODE.0.1.js
---
+++ FindDuplicate,SpaceEditionTM_BEAST-MODE/FindDuplicate,SpaceEditionTM_BEAST-MODE.0.1.js
@@ -0,0 +1,39 @@
+function findDuplicate(intArray) {
+  // Find a number that appears more than once ... in O(n) time
+  let testArr = new Array(intArray.length)
+  for (let i of intArray) {
+    if (testArr[i] === i) return i
+    testArr[i] = i
+  }
+  return 0
+}
+
+// Tests
+
+let desc = "just the repeated number"
+let actual = findDuplicate([1, 1])
+let expected = 1
+assertEqual(actual, expected, desc)
+
+desc = "short array"
+actual = findDuplicate([1, 2, 3, 2])
+expected = 2
+assertEqual(actual, expected, desc)
+
+desc = "medium array"
+actual = findDuplicate([1, 2, 5, 5, 5, 5])
+expected = 5
+assertEqual(actual, expected, desc)
+
+desc = "long array"
+actual = findDuplicate([4, 1, 4, 8, 3, 2, 7, 6, 5])
+expected = 4
+assertEqual(actual, expected, desc)
+
+function assertEqual(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`)
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`)
+  }
+}
